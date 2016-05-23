bd7221278b466d6ae925d195ed9f13528f28e381
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-23 14:34:59 -0700

    implement static page serving

--- server/server.js
+++ server/server.js
@@ -7,9 +7,8 @@ app = express();
 app.get('/', (req, res) => {
 
   // console.log('this is making the request');
-  console.log(__dirname.indexOf('/mvp/'));
-  console.log('another');
-  res.sendFile('index.html', {root: '/../client'});
+  console.log(__dirname.substring(0,  __dirname.indexOf('mvp/')) + 'mvp/client/index.html');
+  res.sendFile('mvp/client/index.html', {root: __dirname.substring(0,  __dirname.indexOf('mvp/'))});
 });
 
 app.listen('8081');
